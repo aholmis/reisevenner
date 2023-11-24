cc8b26401745a0b111b9a3796a5151105ab4157c
Anders Holmefjord <Anders.Holmefjord@dnv.com>
2023-11-24 15:03:03 +0100

    Pages to create and join event

---
+++ src/Data/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorApp1.Data;
+
+public static class Constants
+{
+    public const string AppName = "Reisevenner";
+}
---
+++ src/Data/DriverModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Data;
+
+public class DriverModel
+{
+    public string DriverName { get; set; }
+    public string Phone { get; set; }
+    public int AvailableSpace { get; set; }
+    public List<Passenger> Passengers { get; set; }
+}
+
+public record Passenger(string Name, string Phone);
+
+public record EventModel(string Code, string Name, string When, string Where, List<DriverModel> Drivers);
